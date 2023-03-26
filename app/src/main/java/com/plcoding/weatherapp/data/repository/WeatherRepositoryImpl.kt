db5cb92bfcb4c72cece4e2d3b1d2d8ac624ec458
jiaweiaaa <50084033+jiaweiaaa@users.noreply.github.com>
2023-03-27 05:54:45 +1100

    Front-side done

--- app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,16 +1,28 @@
 package com.plcoding.weatherapp.data.repository
 
 import app.src.main.java.com.plcoding.weatherapp.domain.repository.WeatherRepository
+import com.plcoding.weatherapp.data.mappers.toWeatherInfo
 
 import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import com.plcoding.weatherapp.data.remote.WeatherApi
 import com.plcoding.weatherapp.domain.util.Resource
+import javax.inject.Inject
 
-class WeatherRepositoryImpl (
+class WeatherRepositoryImpl @Inject constructor (
     private val api: WeatherApi
         ): WeatherRepository{
     override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
-        TODO("Not yet implemented")
+        return try {
+            Resource.Success(
+                data = api.getWeatherData(
+                    lat=lat,
+                    long=long
+                ).toWeatherInfo()
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Error(e.message?:"An unknown error occurred")
+        }
     }
         }
 
